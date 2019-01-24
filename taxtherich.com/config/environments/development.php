a8b660c6ae218152cf24eb516d7ab1a55481886d
Kelly Mears <code@kellymears.me>
2019-01-24 10:34:34 -0800

    bad submodule deploy attempt FIX

---
+++ taxtherich.com/config/environments/development.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Configuration overrides for WP_ENV === 'development'
+ */
+
+use Roots\WPConfig\Config;
+
+Config::define('SAVEQUERIES', true);
+Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', false);
+Config::define('SCRIPT_DEBUG', false);
+
+ini_set('display_errors', 1);
+
+// Enable plugin and theme updates and installation from the admin
+Config::define('DISALLOW_FILE_MODS', false);
---
+++ taxtherich.com/web/app/ari-adminer-config.php
@@ -0,0 +1,3 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Access forbidden!' );
+define( 'ARIADMINER_CRYPT_KEY', '99a9b84c87fc30cd2ee16c59a44461a0' );
