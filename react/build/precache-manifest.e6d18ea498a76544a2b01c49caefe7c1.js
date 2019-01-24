0da6aeedaf91188a6b697e972ea5a536d861b893
Kelly Mears <code@kellymears.me>
2019-01-24 10:28:55 -0800

    🚀 Redeploy ancient WordPress

---
+++ react/build/precache-manifest.e6d18ea498a76544a2b01c49caefe7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1d695d684c843b07b012",
+    "url": "/static/js/main.1d695d68.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "1d695d684c843b07b012",
+    "url": "/static/css/main.b9ce9235.chunk.css"
+  },
+  {
+    "revision": "5deced27cff8a3263475b3c6e4fa5932",
+    "url": "/index.html"
+  }
+];
