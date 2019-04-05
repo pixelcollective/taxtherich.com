aed6fbec2f986f579f5b92a2f1e8712e3aad7b6f
Kelly Mears <developers@tinypixel.io>
2019-04-05 02:37:44 -0700

    eek

--- static/source/admin/index.blade.php
+++ static/source/admin/index.blade.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="{{ mix('css/app.css', 'assets/build') }}">
     <title>Admin | {{ $page->site->title }}</title>
+    <link href="config.yml" type="text/yaml" rel="cms-config-url">
 </head>
 <body>
     @foreach ($posts as $post)
     <div class="brand">
-      <img src="images/tax-the-rich.png" class="logo" alt="{!! $post->heading !!}" title="{!! $post->heading !!}" />
+      <img src="assets/images/tax-the-rich.png" class="logo" alt="{!! $post->heading !!}" title="{!! $post->heading !!}" />
     </div>
     @include('_components.hero')
     @include('_components.context')
