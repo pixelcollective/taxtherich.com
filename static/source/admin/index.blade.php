b249f0b8c7f8d85f2d861ce79554d6f5e1ca2a3e
Kelly Mears <developers@tinypixel.io>
2019-04-05 02:32:23 -0700

    deploy!

--- static/source/admin/index.blade.php
+++ static/source/admin/index.blade.php
@@ -3,11 +3,18 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="{{ mix('css/app.css', 'assets/build') }}">
     <title>Admin | {{ $page->site->title }}</title>
-    <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
 </head>
 <body>
-    @yield('content')
+    @foreach ($posts as $post)
+    <div class="brand">
+      <img src="images/tax-the-rich.png" class="logo" alt="{!! $post->heading !!}" title="{!! $post->heading !!}" />
+    </div>
+    @include('_components.hero')
+    @include('_components.context')
+    @include('_components.provisions')
+  @endforeach
 </body>
 </html>
