ddbc8673555f7029f648b6058304dd11badb900e
Kelly Mears <developers@tinypixel.dev>
2019-10-02 02:59:49 -0700

    update build tool

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,11 @@ const Home = ({actions, primary}) => (
     <Action action={primary} />
     <Header
       heading={`The worst of the worst.`}
-      subheading={`While the rest of America toils to meet their tax obligations, the richest of the rich sometimes don't even pay a dime. Send them and your legislators a personalized message letting them know what you think of that.`}
+      subheading={`
+        While the rest of America toils to meet their tax obligations, the richest of the rich
+        sometimes don't even pay a dime. Send them and your legislators a personalized message
+        letting them know what you think of that.
+      `}
       background={`#F4F4F4`}
       color={`#333333`} />
     <Actions actions={actions} />
